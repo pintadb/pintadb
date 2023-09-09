54190cddf1d46a16cb12dd1482686dfe2b771ff2
Itamar <itamarkn60@gmail.com>
2023-09-09 20:13:39 +0300

    feat: Adding FNV hashing

---
+++ db/hash.go
@@ -0,0 +1,18 @@
+package db
+
+// Hash function using the FNV-1a algorithm
+// https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function
+func fowlerNollVo32(s string) uint32 {
+	const (
+		fnvOffset32 uint32 = 2166136261
+		fnvPrime32  uint32 = 16777619
+	)
+
+	hash := fnvOffset32
+	for _, c := range s {
+		hash ^= uint32(c)
+		hash *= fnvPrime32
+	}
+
+	return hash
+}
