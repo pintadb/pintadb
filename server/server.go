99a32b636459206b6b1ee053fb077959189660c7
Itamar <itamarkn60@gmail.com>
2023-09-17 18:08:05 +0300

    feat: Implemet use of http server in main server

--- server/server.go
+++ server/server.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"encoding/gob"
+	"fmt"
 	"math"
 	"os"
 	"path/filepath"
@@ -18,6 +19,8 @@ type PintaDBServer struct {
 type Config struct {
 	FullPath  string
 	Dimension uint64
+	HTTPPort  uint64
+	GRPCPort  uint64
 }
 
 // NewServer creates a new PintaDB server
@@ -27,20 +30,58 @@ func NewServer(config Config) (*PintaDBServer, error) {
 		Config: config,
 	}
 
-	err := os.MkdirAll(filepath.Dir(config.FullPath), os.ModePerm)
-	if err != nil {
-		return nil, err
+	if _, err := os.Stat(config.FullPath); os.IsNotExist(err) {
+		err = createDBFiles(config.FullPath)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		server.LoadDB()
 	}
 
-	file, err := os.Create(server.Config.FullPath)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
+	fmt.Printf("HTTP Server is running on port %d", config.HTTPPort)
+	NewHTTPServer(server, config.HTTPPort)
+	// NewGRPCServer(server, config.GRPCPort)
 
 	return server, nil
 }
 
+func createDBFiles(path string) error {
+	err := os.MkdirAll(filepath.Dir(path), os.ModePerm)
+	if err != nil {
+		return err
+	}
+
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	return nil
+}
+
+// LoadDB loads the database from the file
+func (s *PintaDBServer) LoadDB() error {
+	file, err := s.DBFile()
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	decoder := gob.NewDecoder(file)
+
+	fmt.Println("Loading data from ", s.Config.FullPath)
+	// decoder.Decode(&s.Documents)
+
+	err = decoder.Decode(&s.Documents)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// DBFile returns the file object of the database
 func (s *PintaDBServer) DBFile() (*os.File, error) {
 	file, err := os.OpenFile(s.Config.FullPath, os.O_APPEND|os.O_RDWR, 0600)
 	if err != nil {
@@ -49,9 +90,15 @@ func (s *PintaDBServer) DBFile() (*os.File, error) {
 	return file, nil
 }
 
+// AddDocument adds a new document to the database
+// The document is indexed and vectorized before being added
 func (s *PintaDBServer) AddDocument(text string) error {
 	t := db.TextVec{WordFreq: db.MakeWordFreq(text), Vector: make([]float64, s.Config.Dimension), RawText: text}
 	t.CalculateVector(s.Config.Dimension)
+
+	s.LoadDB()
+	flushFile(s.Config.FullPath)
+
 	s.Documents = append(s.Documents, t)
 
 	file, err := s.DBFile()
@@ -60,7 +107,7 @@ func (s *PintaDBServer) AddDocument(text string) error {
 	}
 	defer file.Close()
 
-	err = encode(file, s.Documents)
+	err = encodeToFile(file, s.Documents)
 	if err != nil {
 		return err
 	}
@@ -69,7 +116,17 @@ func (s *PintaDBServer) AddDocument(text string) error {
 	return nil
 }
 
-func encode(file *os.File, vectors []db.TextVec) error {
+func flushFile(filename string) error {
+	file, err := os.OpenFile(filename, os.O_RDWR|os.O_TRUNC, 0666)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	return nil
+}
+
+func encodeToFile(file *os.File, vectors []db.TextVec) error {
 	encoder := gob.NewEncoder(file)
 	err := encoder.Encode(vectors)
 	return err
@@ -77,24 +134,11 @@ func encode(file *os.File, vectors []db.TextVec) error {
 
 // Query returns the k most similar documents to the given text
 // And the returned documents are sorted by similarity in descending order
-func (s *PintaDBServer) Query(text string, k int) ([]int, error) {
+func (s *PintaDBServer) Search(text string, k int) ([]int, error) {
 	queryVec := db.TextVec{WordFreq: db.MakeWordFreq(text), Vector: make([]float64, s.Config.Dimension)}
 	queryVec.CalculateVector(s.Config.Dimension)
-
-	file, err := s.DBFile()
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	decoder := gob.NewDecoder(file)
-	err = decoder.Decode(&s.Documents)
-	if err != nil {
-		return nil, err
-	}
-
+	s.LoadDB()
 	return calculateResult(queryVec, s.Documents, k), nil
-
 }
 
 func calculateResult(queryVec db.TextVec, documents []db.TextVec, k int) []int {
